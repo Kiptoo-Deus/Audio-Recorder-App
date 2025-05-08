75e385bfb7b4f35f60c9c7277d187d20615d8031
Joel Kiptoo <43814835+Kiptoo-Deus@users.noreply.github.com>
2025-05-08 23:58:51 +0300

    Initial Commit

---
+++ AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Joel on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioRecorderTests/AudioRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderTests.swift
+//  AudioRecorderTests
+//
+//  Created by Joel on 08/05/2025.
+//
+
+import Testing
+@testable import AudioRecorder
+
+struct AudioRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
